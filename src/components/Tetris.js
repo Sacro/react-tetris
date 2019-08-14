62a54d23a5851e5ad66ba53ebd41927fc392245b
Ben Woodward <ben@benwoodward.me.uk>
2019-08-14 22:16:23 +0100

    Adding styles!

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,25 +1,27 @@
 import React from "react";
 
-// Components
+import { createStage } from "../gameHelpers";
 import Display from "./Display";
 import Stage from "./Stage";
 import StartButton from "./StartButton";
+import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
-import { createStage } from "../gameHelpers";
-
+// Components
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
